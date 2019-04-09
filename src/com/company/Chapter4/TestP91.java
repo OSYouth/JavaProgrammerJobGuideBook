8243e7d608e538c644227cdedd784bb8f14f5bdf
benleolee <benleolee@163.com>
2019-04-10 07:39:45 +0800

    新增4.4.3 值传递与引用传递有哪些区别中P91和P92中的示例

---
+++ src/com/company/Chapter4/TestP91.java
@@ -0,0 +1,17 @@
+package com.company.Chapter4;
+
+//4.4.3 值传递与引用传递有哪些区别
+public class TestP91 {
+    public static void testPassParameter(StringBuffer ss1, int n){
+        ss1.append(" World");   //引用
+        n = 8;  //值
+    }
+
+    public static void main(String[] args){
+        int i = 1;
+        StringBuffer s1 = new StringBuffer("Hello");
+        testPassParameter(s1,i);
+        System.out.println(s1);
+        System.out.println(i);
+    }
+}
---
+++ src/com/company/Chapter4/TestP92.java
@@ -0,0 +1,23 @@
+package com.company.Chapter4;
+
+//4.4.3 值传递与引用传递有哪些区别
+public class TestP92 {
+    public static void changeStringBuffer(StringBuffer ss1, StringBuffer ss2){
+        ss1.append(" World");
+        ss2 = ss1;
+    }
+
+    public static void main(String[] args){
+        Integer a = 1;
+        Integer b = a;
+        b ++;
+        System.out.println(a);
+        System.out.println(b);
+
+        StringBuffer s1 = new StringBuffer("Hello");
+        StringBuffer s2 = new StringBuffer("Hello");
+        changeStringBuffer(s1,s2);
+        System.out.println(s1);
+        System.out.println(s2);
+    }
+}
